effe8c9ffa21b118c67acc2e8c12897c5289c0e9
olowo-devops <144071140+olowo-devops@users.noreply.github.com>
2024-04-30 19:07:25 -0400

    Update Nexus.sh

--- Nexus.sh
+++ Nexus.sh
@@ -1,44 +1,35 @@
 #!/bin/bash
-sudo su
-yum update –y
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-amazon-linux-extras install epel -y
-amazon-linux-extras install java-openjdk11 -y
-yum install jenkins -y
-echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-systemctl enable jenkins
-systemctl start jenkins
+yum install java-1.8.0-openjdk.x86_64 wget -y   
+mkdir -p /opt/nexus/   
+mkdir -p /tmp/nexus/                           
+cd /tmp/nexus/
+NEXUSURL="https://download.sonatype.com/nexus/3/latest-unix.tar.gz"
+wget $NEXUSURL -O nexus.tar.gz
+EXTOUT=`tar xzvf nexus.tar.gz`
+NEXUSDIR=`echo $EXTOUT | cut -d '/' -f1`
+rm -rf /tmp/nexus/nexus.tar.gz
+rsync -avzh /tmp/nexus/ /opt/nexus/
+useradd nexus
+chown -R nexus.nexus /opt/nexus 
+cat <<EOT>> /etc/systemd/system/nexus.service
+[Unit]                                                                          
+Description=nexus service                                                       
+After=network.target                                                            
+                                                                  
+[Service]                                                                       
+Type=forking                                                                    
+LimitNOFILE=65536                                                               
+ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
+ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
+User=nexus                                                                      
+Restart=on-abort                                                                
+                                                                  
+[Install]                                                                       
+WantedBy=multi-user.target                                                      
 
-# Installing Git
-yum install git -y
+EOT
 
-# Installing Ansible
-amazon-linux-extras install ansible2 -y
-yum install python-pip -y
-pip install boto3
-
-# Provisioning Ansible Deployer Access
-useradd ansibleadmin
-echo ansibleadmin | passwd ansibleadmin --stdin
-sed -i "s/.*#host_key_checking = False/host_key_checking = False/g" /etc/ansible/ansible.cfg
-sed -i "s/.*#enable_plugins = host_list, virtualbox, yaml, constructed/enable_plugins = aws_ec2/g" /etc/ansible/ansible.cfg
-ansible-galaxy collection install amazon.aws
-
-# Enable Password Authentication and Grant Sudo Privilege
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-systemctl restart sshd
-echo "ansibleadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-# Apache Maven Installation/Config
-#sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-#sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-#sudo yum install -y apache-maven
-#sudo yum install java-1.8.0-devel
-
-#sudo /usr/sbin/alternatives --config java
-#sudo /usr/sbin/alternatives --config javac
-
-# Use The Amazon Linux 2 AMI When Launching The Jenkins VM/EC2 Instance
-# Instance Type: t2.medium or small minimum
-# Open Port (Security Group): 8080 
+echo 'run_as_user="nexus"' > /opt/nexus/$NEXUSDIR/bin/nexus.rc
+systemctl daemon-reload
+systemctl start nexus
+systemctl enable nexus

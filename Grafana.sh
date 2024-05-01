59d384c303f2aab7dded53d898d1df3db9002d1d
olowo-devops <144071140+olowo-devops@users.noreply.github.com>
2024-05-01 19:47:48 -0400

    Updated Grafana.sh

--- Grafana.sh
+++ Grafana.sh
@@ -1,7 +1,35 @@
-sudo apt-get install -y adduser libfontconfig1
-wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
-sudo dpkg -i grafana_7.3.4_amd64.deb
-sudo systemctl daemon-reload
-sudo systemctl start grafana-server
-sudo systemctl status grafana-server
-sudo systemctl enable grafana-server.service
+#!/bin/bash
+
+# Ensure script is executed with root privileges
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+# Add Grafana APT repository
+echo "Adding Grafana APT repository..."
+echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+curl https://packages.grafana.com/gpg.key | apt-key add -
+
+# Update package index
+echo "Updating package index..."
+apt update
+
+# Install Grafana
+echo "Installing Grafana..."
+apt install grafana
+
+# Start Grafana service
+echo "Starting Grafana service..."
+systemctl start grafana-server
+
+# Enable Grafana service to start on boot
+echo "Enabling Grafana service to start on boot..."
+systemctl enable grafana-server
+
+# Display Grafana service status
+echo "Grafana service status:"
+systemctl status grafana-server
+
+# Provide URL to access Grafana
+echo "You can access Grafana by navigating to http://localhost:3000 in your web browser"
